9f1fdefb24cb6f70f8ed013928ce6f5a00d7802a
ph8nt0m <phant0m@kakao.com>
2022-04-29 14:52:55 +0900

    feat: ADD GeneratingFunction

---
+++ QuotesGenerator/Quote.swift
@@ -0,0 +1,13 @@
+//
+//  Quote.swift
+//  QuotesGenerator
+//
+//  Created by 이동준 on 2022/04/29.
+//
+
+import Foundation
+
+struct Quote {
+    let content: String
+    let name: String
+}
--- QuotesGenerator/ViewController.swift
+++ QuotesGenerator/ViewController.swift
@@ -1,4 +1,4 @@
-//
+ //
 //  ViewController.swift
 //  QuotesGenerator
 //
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    let quotes = [
+        Quote(content: "안녕하세요", name: "이동준"),
+        Quote(content: "반갑습니다", name: "손승욱"),
+        Quote(content: "뭐하세요", name: "최지예")
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tagQuoteGeneraterButton(_ sender: Any) {
+        let random = Int(arc4random_uniform(3))
+        let quote = quotes[random]
+        
+        self.quoteLabel.text = quote.content
+        self.nameLabel.text = quote.name
+    }
+    
 }
 
